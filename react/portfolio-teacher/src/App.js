7b2828d8e3856631c57520c4889e354d912eb81b
dahee <kimdahee42@gmail.com>
2024-12-23 10:36:46 +0900

    commit

--- react/portfolio-teacher/src/App.js
+++ react/portfolio-teacher/src/App.js
@@ -4,6 +4,7 @@ import Content from './component/Content.jsx';
 import Footer from './component/Footer.jsx';
 import Logo from './component/header/Logo.jsx';
 import MenuList from './component/header/MenuList.jsx';
+import ToggleButton from './component/header/ToggleButton.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Header>
       <Logo/>
       <MenuList/>
+      <ToggleButton/>
     </Header>
     <Content>
       <h1>Content</h1>
---
+++ react/portfolio-teacher/src/component/header/ToggleButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ToggleButton() {
+  return (
+    <button id="menu_toggle" class="header__toggle" aria-label="navigation menu toggle">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+  );
+}
+
